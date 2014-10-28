c23732054876b8526eae4a52f55f68361cdf20a0
Nayanex <holanda.nayana@gmail.com>
2014-10-28 15:06:37 -0400

    FirstCommit
    Creation of Domain Classes, context through DbContext and Console

---
+++ BreakAwayConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer;
+using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakAwayConsole
+{
+    class Program
+    {
+        private static void InsertDestination()
+        {
+            var destination = new Destination
+            {
+                Country = "Indonesia",
+                Description = "EcoTourism at its best in exquisite Bali",
+                Name = "Bali"
+            };
+
+            using (var context = new BreakAwayContext())
+            {
+                context.Destinations.Add(destination);
+                context.SaveChanges();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            InsertDestination();
+        }
+    }
+}
---
+++ DataLayer/BreakAwayContext.cs
@@ -0,0 +1,16 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class BreakAwayContext : DbContext
+    {
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Lodging> Lodgings { get; set; }
+    }
+}
